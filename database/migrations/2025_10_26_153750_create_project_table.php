620f7e9a867c2762cd553c334e12f969ffb24092
jelainelovendino <jelainelovendino@gmail.com>
2025-10-27 00:19:30 +0800

    db

---
+++ database/migrations/2025_10_26_153750_create_project_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("description");
+            $table->string("file_path");
+            $table->date("upload_date");
+            $table->foreignId("user_id")->constrained("users")->onDelete("cascade");
+            $table->foreignId("category_id")->constrained("category")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project');
+    }
+};
