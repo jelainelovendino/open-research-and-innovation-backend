889c156cd2af9ccfbdb41a490b5270fdb41daccd
jelainelovendino <jelainelovendino@gmail.com>
2025-12-02 20:20:24 +0800

    Update CORS

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000', // local dev
+        env('FRONTEND_URL', 'http://localhost:3000'), // production frontend URL from .env
+    ],
 
     'allowed_headers' => ['*'],
 
