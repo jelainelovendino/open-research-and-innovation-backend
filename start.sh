0f66c9614bd9fefe5813d676862002307a564f54
jelainelovendino <jelainelovendino@gmail.com>
2025-12-05 20:02:28 +0800

    Add storage symlink creation to deployment with error checking

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 php artisan migrate --force
+
+# Create storage symlink if it doesn't exist
+if [ ! -L public/storage ]; then
+  php artisan storage:link
+fi
+
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
